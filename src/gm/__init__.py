0a1bef8d2baff6ceab1f8e62c7f71ea390dbc0ec
Daniel Otto <drotto@uw.edu>
2024-01-31 11:22:09 -1000

    Fixed submodule import

--- src/gm/__init__.py
+++ src/gm/__init__.py
@@ -1,7 +1,9 @@
-import gm.flowline2d
-from gm.gm3s import gm3s
+from .flowline2d import flowline2d
+from .gm3s import gm3s
+from .gm1s import gm1s
 
 __all__ = [
     'flowline2d',
-    'gm3s'
+    'gm3s',
+    'gm1s'
 ]
